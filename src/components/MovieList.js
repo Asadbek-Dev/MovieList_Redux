64b65144d494846fd696569b49fef379abd3c17e
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-09-03 19:31:30 +0500

    completed movielist components

---
+++ src/components/MovieList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectMovie } from '../actions/index';
+
+const MovieList = (props) => {
+    const listItems=props.map((movie)=>{
+        return (
+            <div key={movie.title}>
+                <span>{movie.title}</span>
+                <button onClick={()=>props.selectMovie(movie)}>details</button>
+            </div>
+        )
+    })
+    return (
+        <div className="list-container">
+            <h2>Movie list</h2>
+            <ul>
+                {listItems}
+            </ul>
+        </div>
+    )
+}
+
+const mapStateToProps = (state)=>{
+    return {
+        movies:state.movies
+    }
+}
+
+const mapDispatchToProps={
+    selectMovie:selectMovie
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieList);
